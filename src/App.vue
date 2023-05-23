0f8d93a02254f6aa575ee5e84e129efa6c4c05a4
Khai Thieu <khaithieu3@gmail.com>
2023-05-22 19:30:23 -0500

    modified app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,36 @@
 <template>
-  <p>Hello World</p>
+  <div id="main">
+    <Header></Header>
+    <Filter></Filter>
+    <Ingredient></Ingredient>
+    <Recipe></Recipe>
+  </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Ingredient from "./components/Ingredient.vue";
+import Filter from "./components/Filter.vue";
+import Recipe from "./components/Recipe.vue";
+
 export default {
   name: 'App',
-  components: {}
+  components: {
+    Header, Ingredient, Filter, Recipe
+  }
 }
 </script>
 
-<style>
+<style scoped>
+* {
+  font-family: "Ariel", "Helvetica", "Verdana", "sans-serif";
+  margin: 0;
+  padding: 0;
+}
 
+#main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 </style>
